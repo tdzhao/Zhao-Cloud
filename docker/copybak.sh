b34cd76a9d38b0fa622136aad76cf5be2c06fa57
tdzhao <1354312795@qq.com>
2024-04-07 19:30:04 +0800

    Changes

--- docker/copybak.sh
+++ docker/copybak.sh
@@ -24,6 +24,18 @@ cp ../zhao-gateway/target/zhao-gateway.jar ./zhao/gateway/jar
 echo "begin copy zhao-auth "
 cp ../zhao-auth/target/zhao-auth.jar ./zhao/auth/jar
 
+echo "begin copy zhao-visual "
+cp ../zhao-visual/zhao-monitor/target/zhao-visual-monitor.jar  ./zhao/visual/monitor/jar
+
 echo "begin copy zhao-modules-system "
 cp ../zhao-modules/zhao-system/target/zhao-modules-system.jar ./zhao/modules/system/jar
+
+echo "begin copy zhao-modules-file "
+cp ../zhao-modules/zhao-file/target/zhao-modules-file.jar ./zhao/modules/file/jar
+
+echo "begin copy zhao-modules-job "
+cp ../zhao-modules/zhao-job/target/zhao-modules-job.jar ./zhao/modules/job/jar
+
+echo "begin copy zhao-modules-gen "
+cp ../zhao-modules/zhao-gen/target/zhao-modules-gen.jar ./zhao/modules/gen/jar
 
